d093e8eefcf85f2ddbd7d592869207f3d448fe8e
Maksim Koryukov <maxkoryukov@gmail.com>
2026-04-01 02:37:58 -0400

    chore: fix ascii art / update file headers

--- src/index.spec.mjs
+++ src/index.spec.mjs
@@ -1,20 +1,21 @@
 /*
-################################################################################
-#                                                                              #
-# db    db  .8888.  dP     888888b 8888ba   .8888.     d8b   db 888888b d8888P #
-# 88    88 d8'  `8b 88     88      88  `8b d8'  `8b    88V8  88 88        88   #
-# Y8    8P 88    88 88    a88aaa   88aa8P' 88    88    88 V8 88 88aaa     88   #
-# `8b  d8' 88    88 88     88      88  `8b 88    88    88  V888 88        88   #
-#  `8bd8'  Y8.  .8P 88     88      88  .88 Y8.  .8P dP 88   V88 88        88   #
-#    YP     `888P'  88888P 888888P 888888'  `888P'  88 VP    8P 888888P   dP   #
-#                                                                              #
-################################################################################
+########################################################################
+#                                                                      #
+# db   db  .888.  dP    88888b 888ba   .888.     d8b  db 88888b d8888P #
+# 88   88 d8' `8b 88    88     88 `8b d8' `8b    88V8 88 88       88   #
+# Y8   8P 88   88 88    88aa   88a8P' 88   88    88 VL88 88aa     88   #
+# `8b d8' 88   88 88    88     88 `8b 88   88    88  V88 88       88   #
+#  `8V8'  Y8. .8P 88    88     88 .88 Y8. .8P dP 88  `88 88       88   #
+#   `Y'    `88P'  8888P 88888P 88888'  `88P'  88 VP   8P 88888P   dP   #
+#                                                                      #
+########################################################################
 #
-# Copyright (C) 2016-2025 Volebo <dev@volebo.net>
-# Copyright (C) 2016-2025 Maksim Koryukov <maxkoryukov@volebo.net>
+# Copyright (C) 2016-2026 Volebo <dev@volebo.net>
+# Copyright (C) 2016-2026 Maksim Koryukov <maxkoryukov@volebo.net>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the MIT License, attached to this software package.
+# This program is free software: you can redistribute it
+# and/or modify it under the terms of the MIT License, attached
+# to this software package.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -23,7 +24,9 @@
 # You should have received a copy of the MIT License along with this
 # program. If not, see <https://opensource.org/licenses/MIT>.
 #
-################################################################################
+# http://spdx.org/licenses/MIT
+#
+########################################################################
 */
 
 
--- src/parts/mocha.mjs
+++ src/parts/mocha.mjs
@@ -1,3 +1,34 @@
+/*
+########################################################################
+#                                                                      #
+# db   db  .888.  dP    88888b 888ba   .888.     d8b  db 88888b d8888P #
+# 88   88 d8' `8b 88    88     88 `8b d8' `8b    88V8 88 88       88   #
+# Y8   8P 88   88 88    88aa   88a8P' 88   88    88 VL88 88aa     88   #
+# `8b d8' 88   88 88    88     88 `8b 88   88    88  V88 88       88   #
+#  `8V8'  Y8. .8P 88    88     88 .88 Y8. .8P dP 88  `88 88       88   #
+#   `Y'    `88P'  8888P 88888P 88888'  `88P'  88 VP   8P 88888P   dP   #
+#                                                                      #
+########################################################################
+#
+# Copyright (C) 2016-2026 Volebo <dev@volebo.net>
+# Copyright (C) 2016-2026 Maksim Koryukov <maxkoryukov@volebo.net>
+#
+# This program is free software: you can redistribute it
+# and/or modify it under the terms of the MIT License, attached
+# to this software package.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the MIT License along with this
+# program. If not, see <https://opensource.org/licenses/MIT>.
+#
+# http://spdx.org/licenses/MIT
+#
+########################################################################
+*/
+
 import pluginMocha         from 'eslint-plugin-mocha'
 import globals             from 'globals'
 import pluginChaiFriendly  from 'eslint-plugin-chai-friendly'
